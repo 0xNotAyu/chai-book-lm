6a73ad4c958f34ca0122117ab18526456480e2bc
Aayush sharma <ayutanu1230@gmail.com>
2026-07-26 21:51:39 +0530

    test!

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,15 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-
 export function middleware(req: NextRequest) {
   const res = NextResponse.next();
   if (!req.cookies.get("cbl_uid")) {
-    res.cookies.set("cbl_uid", crypto.randomUUID(), { httpOnly: true, sameSite: "lax", maxAge: 60 * 60 * 24 * 365, path: "/" });
+    res.cookies.set("cbl_uid", crypto.randomUUID(), {
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24 * 365,
+      path: "/",
+    });
   }
   return res;
 }
